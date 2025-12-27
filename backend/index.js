0eca32e0d53e1be7152582ad81c9c14a74d02a7b
SohaSk <150521718+SohaSk@users.noreply.github.com>
2025-12-27 18:17:05 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,23 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 import inventoryRoutes from "./routes/inventory.js";
+
+dotenv.config(); 
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/inventoryDB")
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 app.use("/inventory", inventoryRoutes);
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
